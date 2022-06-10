cabc839b9b6942d3a1b766e4b0eec17a5670c2ea
dundegi <sinpno@naver.com>
2022-06-10 19:15:40 +0900

    조회용 샘플 데이터 입력

--- src/main/java/JpaBook/JpaShop/InitDb.java
+++ src/main/java/JpaBook/JpaShop/InitDb.java
@@ -1,2 +1,89 @@
-package JpaBook.JpaShop;public class InitDb {
+package JpaBook.JpaShop;
+
+import JpaBook.JpaShop.domain.*;
+import JpaBook.JpaShop.domain.item.Book;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+
+@Component
+@RequiredArgsConstructor
+public class InitDb {
+
+    private final InitService initService;
+
+    @PostConstruct
+    public void Init(){
+        initService.dbInit1();
+        initService.dbInit2();
+        //이 곳에다가 코드를 작성해도 될 것 같지만 스프링라이프사이클 때문에 별도로 빈을 설정해줘야한다.
+    }
+
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService{
+
+        private final EntityManager em;
+        public void dbInit1(){
+            Member member = createMember("userA","서울","1111","1111");
+            em.persist(member);
+
+            Book book1 = createBook("JPA1 BOOK", 10000, 100);
+
+            Book book2 = createBook("JPA2 BOOK", 20000, 100);
+
+            OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
+            OrderItem orderItem2 = OrderItem.createOrderItem(book2, 10000, 1);
+
+
+            Delivery delivery = createDelivery(member);
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
+        }
+        public void dbInit2(){
+            Member member = createMember("userB","진주","2222","2222");
+            em.persist(member);
+
+            Book book1 = createBook("Spring1 BOOK", 20000, 200);
+
+            Book book2 = createBook("Spring2 BOOK", 40000, 3g00);
+
+            OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
+            OrderItem orderItem2 = OrderItem.createOrderItem(book2, 10000, 1);
+
+
+            Delivery delivery = createDelivery(member);
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
+        }
+
+        private Delivery createDelivery(Member member) {
+            Delivery delivery = new Delivery();
+            delivery.setAddress(member.getAddress()); //실제 이렇게 안 한다.
+            return delivery;
+        }
+
+        private Book createBook(String JPA1_BOOK, int price, int stockQuantity) {
+            Book book1 = new Book();
+            book1.setName(JPA1_BOOK);
+            book1.setPrice(price);
+            book1.setStockQuantity(stockQuantity);
+            em.persist(book1);
+            return book1;
+        }
+
+        private Member createMember(String name, String city, String street, String zipcode) {
+            Member member = new Member();
+            member.setName(name);
+            member.setAddress(new Address(city, street, zipcode);
+            return member;
+        }
+    }
 }
+
+
